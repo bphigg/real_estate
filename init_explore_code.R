543532312a54523ae0a82fc5c1da7015831446fa
bphigg <bphigg@gmail.com>
2025-04-08 11:18:53 -0400

    added shiny

--- init_explore_code.R
+++ init_explore_code.R
@@ -3,14 +3,14 @@ library(tidyr)
 library(tidyverse)
 
 
-harvest <- read_csv("HomeHarvest.csv")
+harvest <- read_csv("HomeHarvest_radius.csv")
 str(harvest)
 head(harvest)
 harvest <- harvest %>%
   select(property_id, listing_id, mls, mls_id, status, style, street, unit, city, state, zip_code, beds, full_baths, half_baths, sqft, year_built, days_on_mls, list_price, list_date, sold_price, last_sold_date, assessed_value, estimated_value, tax, new_construction, lot_sqft, price_per_sqft, latitude, longitude, neighborhoods, county) %>%
   filter(style != "LAND" & !is.na(days_on_mls))
 
-harvest <- harvest %>% mutate("perc_of_list" = harvest$list_price/harvest$sold_price)
+harvest <- harvest %>% mutate("perc_of_list" = harvest$sold_price/harvest$list_price)
 
 select(harvest, "list_price", "list_price_min", "list_price_max", "sold_price")
 max(harvest$list_price_max)
